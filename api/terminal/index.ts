28939923ea3901ea3afc70e480b946b69d2bac23
Pavel Krymets <pakrym@stripe.com>
2023-03-23 07:35:17 -0700

    Add title to terminal.reader

--- api/terminal/index.ts
+++ api/terminal/index.ts
@@ -114,6 +114,7 @@ export type TerminalReader = {
   ip_address: string | null,
   label: string,
   livemode: boolean,
+  title: string,
   location: TerminalLocation | string | null,
   metadata: unknown /*map*/,
   object: 'terminal.reader',
@@ -405,6 +406,7 @@ export type Endpoints =
       location?: string,
       metadata?: unknown /*map*/ | '',
       registration_code: string,
+      title: string,
     },
     TerminalReader,
   ]
